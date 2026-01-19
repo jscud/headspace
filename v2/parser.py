f3461402e54c265be76fedcc29e166138bdd1693
Jeff Scudder <me@jeffscudder.com>
2026-01-19 15:23:16 -0800

    Simplify parser, pass parent node as parameter

--- v2/parser.py
+++ v2/parser.py
@@ -33,36 +33,32 @@ class Parser:
     self._tokens = tokens
     self._tokens_len = len(tokens)
     self.index = 0
-    self.current_node = None
 
   def current_token(self):
     if self.index >= self._tokens_len:
       return None
     return self._tokens[self.index]
 
-  def process_whitespace(self):
+  def process_whitespace(self, parent_node):
     current_token = self.current_token()
     if current_token.token_type == 'SPACE':
       whitespace_tokens = []
       whitespace_node = Node('SPACES')
       whitespace_node.leaf = True
-      self.current_node.members.append(whitespace_node)
+      parent_node.members.append(whitespace_node)
       while current_token and current_token.token_type == 'SPACE':
         whitespace_node.members.append(current_token.content)
         self.index += 1
         current_token = self.current_token()
 
   def build_parse_tree(self):
-    if not self.current_node:
-      top_node = Node('MODULE')
-      self.current_node = top_node
+    top_node = Node('MODULE')
     if not self._tokens or self._tokens_len == 0:
       return None
     # Consume any leading whitespace.
-    self.process_whitespace()
+    self.process_whitespace(top_node)
     return top_node
     
-
 
 def parse_tokens(tokens):
   parser = Parser(tokens)
