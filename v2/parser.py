f61b108165ff07fba81220df21f03c6f526ed869
Jeff Scudder <me@jeffscudder.com>
2026-01-19 22:12:20 -0800

    Start of parsing function declaration

--- v2/parser.py
+++ v2/parser.py
@@ -63,11 +63,57 @@ class Parser:
         self.index += 1
         current_token = self.current_token()
 
+  def process_function_call(self, parent_node):
+    # Starts with an identifier followed by [.
+    pass
+    
+
+  def process_code_block(self, parent_node):
+    current_token = self.current_token()
+    # We expect the code block to start with an opening [.
+    code_block = Node('CODE_BLOCK')
+    if not current_token or not current_token.matches('SYMBOL', '['):
+      print('Expected a [ to begin a code block')
+      sys.exit(1)
+    code_block.members.append(Node('SYMBOL', [current_token.content], True))
+    self.index += 1
+    self.process_whitespace(code_block)
+    current_token = self.current_token()
+    if current_token and current_token.token_type == 'IDENTIFIER':
+      next_token = self.next_token()
+      if next_token and next_token.matches('SYMBOL', '['):
+        # This is a function/method call.
+        self.process_function_call(code_block)
+    parent_node.members.append(code_block)
+
   def process_function_definition(self, parent_node):
     current_token = self.current_token()
     # The current token is the identifier 'function' to begin the declaration.
-    # TODO
-
+    function_definition = Node('FUNCTION_DEFINITION')
+    if not current_token or not current_token.matches('IDENTIFIER', 'function'):
+      print('function definition did not begin with keyword function')
+      sys.exit(1)
+    function_definition.members.append(Node('FUNCTION_KEYWORD', [current_token.content], True))
+    self.index += 1
+    self.process_whitespace(function_definition)
+    current_token = self.current_token()
+    # Should be an opening [ for the parameter list.
+    if not current_token or not current_token.matches('SYMBOL', '['):
+      print('Expected a [ after the function keyword in function definition')
+      sys.exit(1)
+    function_definition.members.append(Node('SYMBOL', [current_token.content], True))
+    self.index += 1
+    self.process_whitespace(function_definition)
+    # TODO: process the list of parameter declarations.
+    current_token = self.current_token()
+    if not current_token or not current_token.matches('SYMBOL', ']'):
+      print('Expected a ] after the first [ in a function definition')
+      sys.exit(1)
+    function_definition.members.append(Node('SYMBOL', [current_token.content], True))
+    self.index += 1
+    self.process_whitespace(function_definition)
+    self.process_code_block(function_definition) 
+    parent_node.members.append(function_definition)
 
   def process_declaration(self, parent_node):
     current_token = self.current_token()
