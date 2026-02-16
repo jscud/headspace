dd3ccf54403bd3e7ba403ba733018101c297ff6b
Jeff Scudder <me@jeffscudder.com>
2026-02-16 09:37:06 -0800

    convert to Python: hello world test

--- v2/converter.py
+++ v2/converter.py
@@ -92,6 +92,28 @@ class ConverterToPython:
   def __init__(self, parse_tree):
     self.tree = parse_tree
 
+  def emit_function_call(self, function_call_node, py_code, indent_level):
+    if function_call_node.members[0].node_type == 'IDENTIFIER_CHAIN':
+      # Handle a print function.
+      if (function_call_node.members[0].members[0].node_type == 'IDENTIFIER' and
+          function_call_node.members[0].members[0].members[0] == 'os' and
+          function_call_node.members[0].members[2].node_type == 'IDENTIFIER' and
+          function_call_node.members[0].members[2].members[0] == 'print'):
+        py_code.append(' ' * (indent_level))
+        py_code.append('print')
+    if function_call_node.members[1].node_type == 'FUNCTION_CALL_ARGUMENTS':
+      py_code.append('(')
+      if (function_call_node.members[1].members[1].node_type == 'ARGUMENTS' and
+          function_call_node.members[1].members[1].members[0].node_type == 'STRING_LITERAL'):
+        py_code.append(function_call_node.members[1].members[1].members[0].members[0])
+      py_code.append(', end="")')
+
+  def emit_code_block(self, code_block_node, py_code, indent_level):
+    for member in code_block_node.members:
+      if member.node_type == 'FUNCTION_CALL':
+        self.emit_function_call(member, py_code, indent_level + 2)
+    py_code.append('\n')
+
   def emit_code(self):
     py_code = []
     module_name = find_module_name(self.tree)
@@ -103,11 +125,9 @@ class ConverterToPython:
         if member.node_type == 'FUNCTION_DEFINITION':
           for def_member in member.members:
             if def_member.node_type == 'CODE_BLOCK':
-              pass
-              #self.emit_code_block(def_member, py_code)
-        py_code.append('\nif __name__ == \'__main__\':\n  main()\n')
+              self.emit_code_block(def_member, py_code, 0)
+      py_code.append('\nif __name__ == \'__main__\':\n  main()\n')
     return [SourceCodeFile(module_name_py, ''.join(py_code))]
-
 
 
 def convert(parse_tree, target_langauge):
--- v2/tests/converter_test.py
+++ v2/tests/converter_test.py
@@ -25,13 +25,14 @@ class TestConvertToC(unittest.TestCase):
     executable_path = os.path.join('tests', 'test_output', 'hello_test')
     with open(file_path, 'w') as c_source:
       c_source.write(files[0].content)
-    # Then compile and run the c code.
+    # Then compile and run the C code.
     subprocess.run(['gcc', '-Wall', '-Wextra', '-std=c89', '-pedantic',
                     '-Wmissing-prototypes', '-Wstrict-prototypes',
                     '-Wold-style-definition', '-o',
                     executable_path, file_path], check=True)
     result = subprocess.run([executable_path], check=True, capture_output=True)
     self.assertEqual(b'Hello World\n', result.stdout)
+    subprocess.run(['rm', file_path], check=True)
     subprocess.run(['rm', executable_path], check=True)
 
 
@@ -43,6 +44,13 @@ class TestConvertToPython(unittest.TestCase):
     tree = parser.parse_source(HELLO_WORLD_EXAMPLE)
     files = converter.convert(tree, 'python')
     self.assertEqual(1, len(files))
+    file_path = os.path.join('tests', 'test_output', files[0].filename)
+    with open(file_path, 'w') as py_source:
+      py_source.write(files[0].content)
+    # Then execute the Python code.
+    result = subprocess.run(['python3', file_path], check=True, capture_output=True)
+    self.assertEqual(b'Hello World\n', result.stdout)
+    subprocess.run(['rm', file_path], check=True)
 
 
 if __name__ == '__main__':
