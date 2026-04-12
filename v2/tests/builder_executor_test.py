23846c6bdaba99daa267426396da98344674e6e2
Jeff Scudder <me@jeffscudder.com>
2026-04-11 23:35:10 -0700

    Add Go test for building and executing samples with imports

--- v2/tests/builder_executor_test.py
+++ v2/tests/builder_executor_test.py
@@ -54,6 +54,28 @@ class TestBuildPythonAndExecute(unittest.TestCase):
     subprocess.run(['rm', executable_source_path], check=True)
 
 
+class TestBuildGoAndExecute(unittest.TestCase):
+  """Build a headspace project, converting to Go."""
+
+  def test_imports_sample(self):
+    """Multiple files which import and run."""
+    builder.convert_project(os.path.join('tests', 'test_projects', 'import_example'), 'go')
+    go_mod_directory_path = os.path.join('tests', 'test_projects', 'import_example', 'go')
+    library_source_path = os.path.join('tests', 'test_projects', 'import_example', 'go', 'library.go')
+    executable_source_path = os.path.join('tests', 'test_projects', 'import_example', 'python', 'useLibrary', 'main.go')
+    # Generate the go module.
+    os.chdir(go_mod_directory_path)
+    subprocess.run(['go', 'mod', 'init', 'library'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    # Run the program that includes the import.
+    result = subprocess.run(['go', 'run', os.path.join('useLibrary', 'main.go')], check=True, capture_output=True)
+    self.assertEqual(b'Hello from the library\n', result.stdout)
+    # Cleanup compiled files.
+    subprocess.run(['rm', 'go.mod'], check=True)
+    subprocess.run(['rm', 'library.go'], check=True)
+    subprocess.run(['rm', os.path.join('useLibrary', 'main.go')], check=True)
+    os.chdir(os.path.join('..', '..', '..', '..'))
+
+
 if __name__ == '__main__':
   unittest.main()
 
